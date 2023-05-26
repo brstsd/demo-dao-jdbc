e8c0d74737eb67cc2521a7bce89c8657275c3a55
Bianca <bianca-reis2001@hotmail.com>
2023-05-25 22:33:16 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,14 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(1, "Bianca", "bianca@gmail.com", new Date(), 4000.0, obj);
-
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
